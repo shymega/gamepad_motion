b5afdd3581c126f1dbc5e30e2112f592f8f82520
Dom Rodriguez <shymega@shymega.org.uk>
2024-08-03 23:08:14 +0100

    refactor: Deinit submodule, copy files directly

--- build.rs
+++ build.rs
@@ -1,16 +1,6 @@
 #![allow(clippy::too_many_arguments)]
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::process::Command::new("git")
-        .args([
-            "submodule",
-            "update",
-            "--init",
-            "--depth=1",
-            "--recommend-shallow",
-        ])
-        .output()
-        .expect("Failed to fetch git submodules!");
     let path = std::path::PathBuf::from("lib/GamepadMotionHelpers");
     let mut b = autocxx_build::Builder::new("src/lib.rs", [&path])
         .extra_clang_args(&["-std=c++17"])
